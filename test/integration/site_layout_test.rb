571313abb24321ae4b6ac6c5dd3cc5b0b17ecca7
vamshi-tg <vamshi.tg@beautifulcode.in>
2019-05-28 12:05:58 +0530

    Completed user management

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -18,5 +18,19 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
     get signup_path
     assert_select "title", full_title("Sign up")
+
+    @user = users(:test_user_1)
+    log_in_as(@user)
+    
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,5 +12,18 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
   # Add more helper methods to be used by all tests here...
+end
+
+class ActionDispatch::IntegrationTest
+
+  # Log in as a particular user.
+  def log_in_as(user, password: 'password')
+    post login_path, params: { session: { email: user.email,
+                                          password: password } }
+  end
 end
